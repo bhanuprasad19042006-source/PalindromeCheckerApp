ed1d422e4973136ba7a3bf4f06721474c4d5b194
bhanuprasad19042006 <bhanuprasd19042006@gmail.com>
2026-02-12 10:29:00 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker" + "Management System");
+
+    }
+}
